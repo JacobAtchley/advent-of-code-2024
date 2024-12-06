1b4d7b5026a66e1cfae1fd859d6c7a89c83647df
Jacob Atchley <yaaqob.atchley@gmail.com>
2024-12-06 15:39:40 -0600

    feat: day one

---
+++ Advent/Advent/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine($"Day 1.1 {Advent.Days._1.Solution.SolvePartOne()}");
+Console.WriteLine($"Day 1.2 {Advent.Days._1.Solution.SolvePartTwo()}");
