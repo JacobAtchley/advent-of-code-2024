0e912b196c523166749ca95c26c834811fb56447
Jacob Atchley <yaaqob.atchley@gmail.com>
2024-12-06 19:47:16 -0600

    feat: day 2 part 2

--- Advent/Advent/Days/2/Solution.cs
+++ Advent/Advent/Days/2/Solution.cs
@@ -1,36 +1,79 @@
 namespace Advent.Days._2;
 
+public record Diff(int Index, int Difference)
+{
+    public bool IsAscending = Difference > 0;
+    public bool IsDescending = Difference < 0;
+    public int Absolute = Math.Abs(Difference);
+    public bool IsSafe => Absolute is >= 1 and <= 3;
+}
+
 public class Solution
 {
     public class Report(string[] valueStrings)
     {
         private int[]? _values;
         public int[] GetValues() => _values ??= valueStrings.Select(int.Parse).ToArray();
-        
-        public bool IsSafe()
-        {
-            var ints = GetValues();
 
+        private static bool ComputeSafe(int[] ints, int? skipIndex = null)
+        {
             if (ints.Length < 2)
             {
                 return true;
             }
-
-            var diffs = ints
-                .Select((value, index) => new { value, index })
-                .Where(x => x.index > 0)
-                .Select(x => ints[x.index - 1] - x.value)
-                .ToArray();
             
-            return diffs.All(x => x is >= 1 and <= 3) || diffs.All(x => x is >= -3 and <= -1);
+            var values = skipIndex is null
+                ? ints
+                : ints.Where((_, index) => index != skipIndex).ToArray();
+
+            var diffs = values
+                .Select((value, index) => new { value, index })
+                .Where(x => x.index != 0)
+                .Select(x => new Diff(x.index, values[x.index - 1] - x.value))
+                .ToArray();
+
+            var isAllAscending = diffs.All(x => x.IsAscending);
+            var isAllDescending = diffs.All(x => x.IsDescending);
+            var isAllSafe = diffs.All(x => x.IsSafe);
+
+            var isSafe = (isAllAscending || isAllDescending) && isAllSafe;
+
+            return isSafe;
+        }
+
+        public bool IsSafe(bool useDampener)
+        {
+            var ints = GetValues();
+            var isSafe = ComputeSafe(ints);
+
+            if (useDampener is false)
+            {
+                return isSafe;
+            }
+            
+            for (var i = 0; i < ints.Length; i++)
+            {
+                if (ComputeSafe(ints, i))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
-
-    public static string SolvePartOne()
+    
+    private static string Solve(bool useDapmener)
         => File
             .ReadAllLines("Days/2/input.txt")
             .Select(x => x.Split(' '))
             .Select(x => new Report(x))
-            .Count(x => x.IsSafe())
+            .Count(x => x.IsSafe(useDapmener))
             .ToString();
+
+    public static string SolvePartOne()
+        => Solve(false);
+    
+    public static string SolvePartTwo()
+        => Solve(true);
 }
--- Advent/Advent/Program.cs
+++ Advent/Advent/Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine($"Day 1.1 {Advent.Days._1.Solution.SolvePartOne()}");
 Console.WriteLine($"Day 1.2 {Advent.Days._1.Solution.SolvePartTwo()}");
 Console.WriteLine($"Day 2.1 {Advent.Days._2.Solution.SolvePartOne()}");
+Console.WriteLine($"Day 2.2 {Advent.Days._2.Solution.SolvePartTwo()}");
